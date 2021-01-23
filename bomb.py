4a964b41c0d0b76f38109e21adcfd25e21db3fd7
Matrer <koziol.marcin96@gmail.com>
2021-01-23 13:01:03 +0100

    # add gitignore # change style of explosion # change tnt texutre

--- bomb.py
+++ bomb.py
@@ -1,35 +1,45 @@
 from ursina import *
 
+distance_y = -0.2
+
+distance_x = 2.1
+
 
 class Explosion(Entity):
-    def explode(self):
+    def explode(self, walls):
         destroy(self.parent)
 
-    def __init__(self, parent):
+    def __init__(self, parent, walls, scale=1, position=(0, 0, 0)):
         super().__init__(
             parent=parent,
-            position=(0, 0, 0),
+            position=[x * scale for x in position],
+            scale=scale,
             model='sphere',
+            collider='sphere',
             texture='l0',
             color=color.white,
         )
-        invoke(self.explode, delay=1)
+        for wall in walls:
+            if wall.intersects(self).hit:
+                destroy(wall)
+        invoke(self.explode, walls, delay=.5)
 
 
 class Bomb(Entity):
     def explode(self, walls):
-        for wall in walls:
-            if wall.intersects(self).hit:
-                destroy(wall)
-        Explosion(self)
+        Explosion(self, walls, .9)
+        for i in range(4):
+            Explosion(self, walls, .5 / (i+1), (i * distance_x + 1, distance_y * i, 0))
+            Explosion(self, walls, .5 / (i+1), (-i * distance_x - 1, distance_y * i, 0))
+            Explosion(self, walls, .5 / (i+1), (0, distance_y * i, i * distance_x + 1))
+            Explosion(self, walls, .5 / (i+1), (0, distance_y * i, -i * distance_x - 1))
 
     def __init__(self, walls, position=(0, 0, 0)):
         super().__init__(
             parent=scene,
             position=position,
             model='bomb',
-            collider='sphere',
-            scale=4,
+            scale=3,
             texture='tnt',
             color=color.white,
             highlight_color=color.olive,
