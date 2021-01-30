87a596afc53176b8350a028da10307bd09613f7f
Marcin Bąk <marcbak752@student.polsl.pl>
2021-01-30 10:35:29 +0100

    # changed structure of the main.py file

--- main.py
+++ main.py
@@ -7,54 +7,35 @@ from bomber import Bomber
 from constants import WORLD_SCALE
 from myFirstPersonController import MyFirstPersonController
 
-app = Ursina()
-current_map = [
-#start
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
-    [2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 2],
-    [2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2],
-    [2, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 2],
-    [2, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 2],
-    [2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
-    [2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
-    [2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 2],
-    [2, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 2],
-    [2, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 2],
-    [2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 2],
-    [2, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 2],
-    [2, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2],
-    [2, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
-    [2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
-    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-]
-
-world_size_x = len(current_map[0])
-world_size_z = len(current_map)
-Texture.default_filtering = None
-
-window.fps_counter.enabled = True
-window.exit_button.visible = False
-
-# Audio files
-snd_bg = Audio('./snd/Factory.ogg', pitch=1, loop=True, autoplay=True)
-snd_putbomb = Audio('./snd/Pickup_Coin4.wav', pitch=1, loop=False, autoplay=False)
-player = MyFirstPersonController()
-
 # Create menu
-DropdownMenu('Menu', buttons=(
-    DropdownMenuButton('New'),
-    DropdownMenuButton('Options'),
-    DropdownMenu('Options', buttons=(
-        DropdownMenuButton('Option a'),
-        DropdownMenuButton('Option b'),
-    )),
-    DropdownMenuButton('Exit'),
-))
+class Menu(DropdownMenu):
+    def __init__(self, name):
+        super().__init__(
+            text=name,
+            buttons=(
+                DropdownMenuButton('New'),
+                DropdownMenu('Options', buttons=(
+                    DropdownMenuButton('Option a'),
+                    DropdownMenuButton('Option b'),
+                )),
+                DropdownMenu('Help'),
+                DropdownMenuButton('Exit'),
+            ),
+        )
+        self.is_open = False
 
+    def input(self, key):
+        if key == 'tab':
+            if self.is_open == False:
+                self.open()
+                self.is_open = True
+            else:
+                self.close()
+                self.is_open = False
+        if key == 'arrow_right':
+            ...
+    def update(self):
+        ...
 
 class Skybox(Entity):
     def __init__(self):
@@ -119,25 +100,64 @@ class HardWall(Button):
         )
 
 
-walls = []
-for z in range(world_size_z):
-    for x in range(world_size_x):
-        Ground(scene, (x * WORLD_SCALE, 0, z * WORLD_SCALE))
-        if current_map[z][x] == 1:
-            walls.append(Wall((x * WORLD_SCALE, 1 * WORLD_SCALE, z * WORLD_SCALE)))
-        if current_map[z][x] == 2:
-            HardWall((x * WORLD_SCALE, 1 * WORLD_SCALE, z * WORLD_SCALE))
+if __name__ == '__main__':
 
-enemy_table = [
-    Bomber(scene, (1 * WORLD_SCALE, 1 * WORLD_SCALE, 1 * WORLD_SCALE)),
-    Bomber(scene, (18 * WORLD_SCALE, 1 * WORLD_SCALE, 18 * WORLD_SCALE)),
-    Bomber(scene, (1 * WORLD_SCALE, 1 * WORLD_SCALE, 18 * WORLD_SCALE))
-]
+    app = Ursina()
+    current_map = [
+        # start
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 2],
+        [2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2],
+        [2, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 2],
+        [2, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 2],
+        [2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
+        [2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
+        [2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 2],
+        [2, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 2],
+        [2, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 2],
+        [2, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 2],
+        [2, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 2],
+        [2, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2],
+        [2, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 2],
+        [2, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 2],
+        [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    ]
 
-scene.walls = walls
-scene.app = app
-scene.player = player
-scene.enemy_table = enemy_table
-skybox = Skybox()
+    world_size_x = len(current_map[0])
+    world_size_z = len(current_map)
+    Texture.default_filtering = None
 
-app.run()
+    window.fps_counter.enabled = True
+    window.exit_button.visible = False
+
+    # Audio files
+    snd_bg = Audio('./snd/Factory.ogg', pitch=1, loop=True, autoplay=True)
+    snd_putbomb = Audio('./snd/Pickup_Coin4.wav', pitch=1, loop=False, autoplay=False)
+    player = MyFirstPersonController()
+
+    walls = []
+    for z in range(world_size_z):
+        for x in range(world_size_x):
+            Ground(scene, (x * WORLD_SCALE, 0, z * WORLD_SCALE))
+            if current_map[z][x] == 1:
+                walls.append(Wall((x * WORLD_SCALE, 1 * WORLD_SCALE, z * WORLD_SCALE)))
+            if current_map[z][x] == 2:
+                HardWall((x * WORLD_SCALE, 1 * WORLD_SCALE, z * WORLD_SCALE))
+
+    enemy_table = [
+        Bomber(scene, (1 * WORLD_SCALE, 1 * WORLD_SCALE, 1 * WORLD_SCALE)),
+        Bomber(scene, (18 * WORLD_SCALE, 1 * WORLD_SCALE, 18 * WORLD_SCALE)),
+        Bomber(scene, (1 * WORLD_SCALE, 1 * WORLD_SCALE, 18 * WORLD_SCALE))
+    ]
+
+    scene.walls = walls
+    scene.app = app
+    scene.player = player
+    scene.enemy_table = enemy_table
+    skybox = Skybox()
+
+    app.run()
